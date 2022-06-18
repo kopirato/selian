7328f802c846ed6b4038d0465b1e42e902a0cc80
kopirato <84496338+kopirato@users.noreply.github.com>
2022-06-18 06:52:33 +0300

    Update touch bot.js

--- touch bot.js
+++ touch bot.js
@@ -1,6 +1,9 @@
-const { auth } = require('./config.js');
+
+
+const { auth, postReplyWithMedia, postReply  } = require('./config.js');
 
 const client = auth();
+
 
 client.stream('statuses/filter', { track: '#FilterMe' }, function (stream) {
   console.log("Searching for tweets...");
@@ -10,7 +13,16 @@ client.stream('statuses/filter', { track: '#FilterMe' }, function (stream) {
     console.log("Found one!");
     console.log("Recieved tweet reading...", tweet.text);
 
-    // some code to respond to the tweet
+    // check if tweet contains some media
+    if (tweet.media_url) {
+      console.log("Replying to tweet with video.");
+      postReplyWithMedia(client, "./sample-media/video.mp4", tweet);
+
+    } else {
+      console.log("Tweet didn't provide media. Replying with message.");
+      const message = "Oops, looks like you didn't provide a media file!";
+      postReply(client, message, tweet);
+    }
 
     stream.on('error', function (error) {
       console.log(error);

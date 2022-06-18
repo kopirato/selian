c21ea46a070aa1c4fdfd46698e75d917c144e240
kopirato <84496338+kopirato@users.noreply.github.com>
2022-06-18 06:39:03 +0300

    Update touch bot.js

--- touch bot.js
+++ touch bot.js
@@ -1,1 +1,19 @@
+const { auth } = require('./config.js');
 
+const client = auth();
+
+client.stream('statuses/filter', { track: '#FilterMe' }, function (stream) {
+  console.log("Searching for tweets...");
+
+  // when a tweet is found
+  stream.on('data', function (tweet) {
+    console.log("Found one!");
+    console.log("Recieved tweet reading...", tweet.text);
+
+    // some code to respond to the tweet
+
+    stream.on('error', function (error) {
+      console.log(error);
+    });
+  });
+});
